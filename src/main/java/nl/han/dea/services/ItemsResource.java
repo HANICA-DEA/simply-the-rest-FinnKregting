c1abd78d0865f9d0eea2840be31d0c059023ce2c
FinnKregting <FinnFoxkregting@gmail.com>
2021-02-26 13:14:51 +0100

    t/m Opgv 8

--- src/main/java/nl/han/dea/services/ItemsResource.java
+++ src/main/java/nl/han/dea/services/ItemsResource.java
@@ -1,22 +1,51 @@
 package nl.han.dea.services;
 
+import nl.han.dea.services.dto.ItemDTO;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/items")
 public class ItemsResource {
+    private ItemService itemService;
+
+    public ItemsResource() {
+        this.itemService = new ItemService();
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getItems() {
         return "bread, butter";
     }
 
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public String getItemsJSON() {
+//        return "[\"bread\", \"butter\"]";
+//    }
+
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public List<ItemDTO> getItemsJSON() {
+//        return itemService.getAll();
+//    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJsonItems() {
-        return "[\"bread\", \"butter\"]";
+    public Response getItemsJSON() {
+        return Response.ok().entity(itemService.getAll()).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getItem(@PathParam("id") int id) {
+        return Response.ok().entity(itemService.getItem(id)).build();
     }
 }
