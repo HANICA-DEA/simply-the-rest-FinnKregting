6fafd90321881f40a31a7d7b9dffe70583688b9a
FinnKregting <FinnFoxkregting@gmail.com>
2021-03-02 16:51:36 +0100

    Opgv 1 t/m 11

--- src/main/java/nl/han/dea/services/ItemsResource.java
+++ src/main/java/nl/han/dea/services/ItemsResource.java
@@ -2,27 +2,26 @@ package nl.han.dea.services;
 
 import nl.han.dea.services.dto.ItemDTO;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ejb.Singleton;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/items")
+@Singleton
 public class ItemsResource {
     private ItemService itemService;
 
     public ItemsResource() {
         this.itemService = new ItemService();
     }
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getItems() {
-        return "bread, butter";
-    }
+//
+//    @GET
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String getItems() {
+//        return "bread, butter";
+//    }
 
 //    @GET
 //    @Produces(MediaType.APPLICATION_JSON)
@@ -48,4 +47,21 @@ public class ItemsResource {
     public Response getItem(@PathParam("id") int id) {
         return Response.ok().entity(itemService.getItem(id)).build();
     }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addItem(ItemDTO itemDTO) {
+        itemService.addItem(itemDTO);
+        return Response.status(201).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteItem(@PathParam("id") int id) {
+        itemService.deleteItem(id);
+        System.out.println(itemService);
+        return Response.ok().build();
+    }
+
 }
